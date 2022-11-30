237eccf65836c94849a33ebb90639ab22c9d6549
quilcummings <quil735@gmail.com>
2022-11-29 20:59:19 -0700

    created react app skeleton

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          REACT TEST
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
