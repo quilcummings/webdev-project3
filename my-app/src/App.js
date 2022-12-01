bb844c875714b5ddf8b12d035d2cccb0b7686a7a
quilcummings <quil735@gmail.com>
2022-12-01 11:19:03 -0700

    added cards to flex container

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,11 +5,42 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">  
-      <Card/>
-      <Card/>
+    <div class="flex-container">
+            <div class ="flex-item" id="first-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="second-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="third-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+            <div class ="flex-item" id="fourth-child">
+              <Card/>
+            </div>
+        </div>   
+      
+     
     </div>
   );
 }
-
 
 export default App;
