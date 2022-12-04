869d5a7b25455b0db2d9caf514f385e984f558f2
Sandy Lee <sl30724@gmail.com>
2022-12-04 16:49:48 -0700

    Added flip state

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -16,13 +16,40 @@ export default function Card(props) {
     );
 }*/
 export default class Card extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            flip: false,
+        };
+    }
+    
+    // handleClick
+    handleClick = () => {
+        // check previous state to see if the card is already flipped
+        this.setState(prevState => ({
+            flip: !prevState.flip,
+        }));
+        // console.log(this.state.flip);
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.flip);
+    }
+
     render() {
         return (
 
             <div className='card'>
                 <div className='card-front'>
-                    <img src={this.props.path} alt={this.props.path} />
+                    <img
+                        src={this.props.path}
+                        alt={this.props.path}
+                        onClick={()=> {
+                         this.handleClick();
+                         }} />
                 </div>
+                <p></p>
                 <div className='card-back'>
                     <p>Back of Card</p>
                 </div>
